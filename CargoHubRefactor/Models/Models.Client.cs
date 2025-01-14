f1c94ddd102a04db5c56e00ff546c51b55c06ff2
Efroem <134206024+Efroem@users.noreply.github.com>
2025-01-14 14:04:59 +0100

    Update Models.Client.cs

--- CargoHubRefactor/Models/Models.Client.cs
+++ CargoHubRefactor/Models/Models.Client.cs
@@ -3,15 +3,15 @@ using System;
 public class Client
 {
     public int ClientId { get; set; }
-    public string Name { get; set; }
-    public string Address { get; set; }
-    public string City { get; set; }
-    public string ZipCode { get; set; }
-    public string Province { get; set; }
-    public string Country { get; set; }
-    public string ContactName { get; set; }
-    public string ContactPhone { get; set; }
-    public string ContactEmail { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+    public string ZipCode { get; set; } = string.Empty;
+    public string Province { get; set; } = string.Empty;
+    public string Country { get; set; } = string.Empty;
+    public string ContactName { get; set; } = string.Empty;
+    public string ContactPhone { get; set; } = string.Empty;
+    public string ContactEmail { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
 }
